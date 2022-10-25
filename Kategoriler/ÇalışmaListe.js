ac0996b661d2a72702fc5806912dbed2689c5197
MFY60 <101919771+MFY60@users.noreply.github.com>
2022-10-25 08:01:22 +0300

    Röportaj listesi başlangıç

---
+++ Kategoriler/ÇalışmaListe.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    // Koyu tema
+    var prevAktifTheme = localStorage.getItem("theme-color");
+    document.documentElement.setAttribute("data-theme", prevAktifTheme ? prevAktifTheme : "light");
+    var themeToggle = document.getElementsByClassName('TemaRengiButon')[0];
+    themeToggle.onclick = function () {
+        var currentTheme = document.documentElement.getAttribute("data-theme");
+        var switchToTheme = currentTheme === "dark" ? "light" : "dark";
+        localStorage.setItem("theme-color", switchToTheme)
+        document.documentElement.setAttribute("data-theme", switchToTheme);
+    }
+
+    // Animasyon
+    AOS.init({
+        once: true,
+        offset: 10,
+        duration: 600,
+        easing: 'cubic-bezier(0.42, 0, 0.12, 1.28)'
+    });
+
+    // İmleç
+    new kursor({
+        type: 4,
+        color: '#FFC024'
+    });
+
+    // Yumuşak kaydırma
+    document.querySelectorAll('.Başlık .BaşlıkÇubuğu .BaşlıkÇubuğuLinkler a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                block: "start"
+            });
+        });
+    });
+})
