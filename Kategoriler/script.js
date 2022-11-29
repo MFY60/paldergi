59afea7347c4a6d8073d5c141880a422e69b9fad
MFY60 <101919771+MFY60@users.noreply.github.com>
2022-11-29 21:56:53 +0300

    Image slider and Genç yeryüzü doktorları eklendi, video player hata düzeltmeleri

--- Kategoriler/script.js
+++ Kategoriler/script.js
@@ -37,4 +37,37 @@ document.addEventListener("DOMContentLoaded", function (event) {
             });
         });
     });
+
+    //Swiper slide
+    var swiper = new Swiper(".slide-content", {
+        slidesPerView: 3,
+        spaceBetween: 25,
+        loop: false,
+        centerSlide: 'true',
+        fade: 'true',
+        grabCursor: 'true',
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          dynamicBullets: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+    
+        breakpoints:{
+            0: {
+                slidesPerView: 1,
+            },
+            520: {
+                slidesPerView: 2,
+            },
+            950: {
+                slidesPerView: 3,
+            },
+        },
+      });
+
+
 })
