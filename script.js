46b1532a6e2ad97392f7af1ad46c1346c6ea4ef8
MFY60 <101919771+MFY60@users.noreply.github.com>
2022-12-18 11:39:35 +0300

    Anasayfa scriptinin güvenilirliği iyileştirildi

--- script.js
+++ script.js
@@ -1,70 +1,94 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     // Koyu tema
-    var prevAktifTheme = localStorage.getItem("theme-color");
-    document.documentElement.setAttribute("data-theme", prevAktifTheme ? prevAktifTheme : "light");
-    var themeToggle = document.getElementsByClassName('TemaRengiButon')[0];
-    themeToggle.onclick = function () {
-        var currentTheme = document.documentElement.getAttribute("data-theme");
-        var switchToTheme = currentTheme === "dark" ? "light" : "dark";
-        localStorage.setItem("theme-color", switchToTheme)
-        document.documentElement.setAttribute("data-theme", switchToTheme);
+    try {
+        var prevAktifTheme = localStorage.getItem("theme-color");
+        document.documentElement.setAttribute("data-theme", prevAktifTheme ? prevAktifTheme : "light");
+        var themeToggle = document.getElementsByClassName('TemaRengiButon')[0];
+        themeToggle.onclick = function () {
+            var currentTheme = document.documentElement.getAttribute("data-theme");
+            var switchToTheme = currentTheme === "dark" ? "light" : "dark";
+            localStorage.setItem("theme-color", switchToTheme)
+            document.documentElement.setAttribute("data-theme", switchToTheme);
+        }
+    } catch (error) {
     }
 
     // Animasyon
-    AOS.init({
-        once: true,
-        offset: 10,
-        duration: 600,
-        easing: 'cubic-bezier(0.42, 0, 0.12, 1.28)'
-    });
+    try {
+        AOS.init({
+            once: true,
+            offset: 10,
+            duration: 600,
+            easing: 'cubic-bezier(0.42, 0, 0.12, 1.28)'
+        });
+    } catch (error) {
+    }
 
     // SVG desteği
-    svg4everybody();
+    try {
+        svg4everybody();
+    } catch (error) {
+    }
 
     // CSS değişken desteği
-    cssVars({});
+    try {
+        cssVars({});
+    } catch (error) {
+    }
 
     // Yapışkan menü
-    var menu = document.getElementsByClassName("Başlık")[0];
-    if (window.pageYOffset >= 32) {
-        menu.classList.add('sticky');
-    }
-    var lastScroll = 0;
-    window.addEventListener("scroll", function () {
-        var currentScroll = window.pageYOffset;
-        if (currentScroll <= 32) {
-            menu.classList.remove('sticky');
-            return;
-        } else {
+    try {
+        var menu = document.getElementsByClassName("Başlık")[0];
+        if (window.pageYOffset >= 32) {
             menu.classList.add('sticky');
         }
-        lastScroll = currentScroll;
-    });
+        var lastScroll = 0;
+        window.addEventListener("scroll", function () {
+            var currentScroll = window.pageYOffset;
+            if (currentScroll <= 32) {
+                menu.classList.remove('sticky');
+                return;
+            } else {
+                menu.classList.add('sticky');
+            }
+            lastScroll = currentScroll;
+        });
+    } catch (error) {
+    }
 
     // Yumuşak kaydırma
-    document.querySelectorAll('.Başlık .BaşlıkÇubuğu .BaşlıkÇubuğuLinkler a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth',
-                block: "start"
+    try {
+        document.querySelectorAll('.Başlık .BaşlıkÇubuğu .BaşlıkÇubuğuLinkler a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth',
+                    block: "start"
+                });
             });
         });
-    });
+    } catch (error) {
+    }
 
     // En yukarı git
-    var trigger = document.getElementsByClassName('Logo')[0];
-    trigger.onclick = function () {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
+    try {
+        var trigger = document.getElementsByClassName('Logo')[0];
+        trigger.onclick = function () {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        }
+    } catch (error) {
     }
 
     // Mobil menü
-    var mobileMenuToggle = document.getElementsByClassName('MobilMenüButon')[0];
-    mobileMenuToggle.onclick = function () {
-        document.querySelector(".Başlık .BaşlıkÇubuğu .BaşlıkÇubuğuLinkler").classList.toggle('Aktif');
-    }
+    try {
+        var mobileMenuToggle = document.getElementsByClassName('MobilMenüButon')[0];
+        mobileMenuToggle.onclick = function () {
+            document.querySelector(".Başlık .BaşlıkÇubuğu .BaşlıkÇubuğuLinkler").classList.toggle('Aktif');
 
-    window.onscroll = function (e) {
-        document.querySelector(".Başlık .BaşlıkÇubuğu .BaşlıkÇubuğuLinkler").classList.remove('Aktif');
+            window.onscroll = function (e) {
+                document.querySelector(".Başlık .BaşlıkÇubuğu .BaşlıkÇubuğuLinkler").classList.remove('Aktif');
+            }
+        }
+    } catch (error) {
     }
 })
