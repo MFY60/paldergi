b9bade1a1d107426acb2f4d3aa821fe6bf274847
MFY60 <101919771+MFY60@users.noreply.github.com>
2022-12-18 11:34:35 +0300

    Değişik bug ve script güvenilirliği

--- kategoriler/script.js
+++ kategoriler/script.js
@@ -1,57 +1,68 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     // Koyu tema
-    var prevAktifTheme = localStorage.getItem("theme-color");
-    document.documentElement.setAttribute("data-theme", prevAktifTheme ? prevAktifTheme : "light");
+    try {
+        var prevAktifTheme = localStorage.getItem("theme-color");
+        document.documentElement.setAttribute("data-theme", prevAktifTheme ? prevAktifTheme : "light");
+    } catch (error) {
+    }
 
     // Animasyon
-    AOS.init({
-        once: true,
-        offset: 10,
-        duration: 600,
-        easing: 'cubic-bezier(0.42, 0, 0.12, 1.28)'
-    });
+    try {
+        AOS.init({
+            once: true,
+            offset: 10,
+            duration: 600,
+            easing: 'cubic-bezier(0.42, 0, 0.12, 1.28)'
+        });
+    } catch (error) {
+    }
 
     // Yumuşak kaydırma
-    document.querySelectorAll('.Başlık .BaşlıkÇubuğu .BaşlıkÇubuğuLinkler a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth',
-                block: "start"
+    try {
+        document.querySelectorAll('.Başlık .BaşlıkÇubuğu .BaşlıkÇubuğuLinkler a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth',
+                    block: "start"
+                });
             });
         });
-    });
+    } catch (error) {
+    }
 
     //Swiper slide
-    var swiper = new Swiper(".slide-content", {
-        slidesPerView: 3,
-        spaceBetween: 25,
-        loop: false,
-        centerSlide: 'true',
-        fade: 'true',
-        grabCursor: 'true',
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-          dynamicBullets: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-    
-        breakpoints:{
-            0: {
-                slidesPerView: 1,
+    try {
+        var swiper = new Swiper(".slide-content", {
+            slidesPerView: 3,
+            spaceBetween: 25,
+            loop: false,
+            centerSlide: 'true',
+            fade: 'true',
+            grabCursor: 'true',
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+                dynamicBullets: true,
             },
-            520: {
-                slidesPerView: 2,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
             },
-            950: {
-                slidesPerView: 3,
-            },
-        },
-      });
 
+            breakpoints: {
+                0: {
+                    slidesPerView: 1,
+                },
+                520: {
+                    slidesPerView: 2,
+                },
+                950: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+    } catch (error) {
+    }
 
 })
