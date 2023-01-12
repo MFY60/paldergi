faf966b396becff3597ebb3f3497e7a17a7c6564
MFY60 <meliksah_yalcin@icloud.com>
2023-01-12 23:49:45 +0300

    Pendik Anadolu Lisesi Kupalara Doymuyor

--- kategoriler/script.js
+++ kategoriler/script.js
@@ -38,8 +38,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
             spaceBetween: 25,
             loop: false,
             centerSlide: 'true',
+            centerInsufficientSlides: 'true',
             fade: 'true',
             grabCursor: 'true',
+            autoHeight: 'true',
             pagination: {
                 el: ".swiper-pagination",
                 clickable: true,
@@ -48,6 +50,358 @@ document.addEventListener("DOMContentLoaded", function (event) {
             navigation: {
                 nextEl: ".swiper-button-next",
                 prevEl: ".swiper-button-prev",
+            },
+
+            breakpoints: {
+                0: {
+                    slidesPerView: 1,
+                },
+                520: {
+                    slidesPerView: 2,
+                },
+                950: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+        
+        var swiper1 = new Swiper(".slide-content1", {
+            slidesPerView: 3,
+            spaceBetween: 25,
+            loop: false,
+            centerSlide: 'true',
+            centerInsufficientSlides: 'true',
+            fade: 'true',
+            grabCursor: 'true',
+            autoHeight: 'true',
+            pagination: {
+                el: ".swiper-pagination1",
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next1",
+                prevEl: ".swiper-button-prev1",
+            },
+
+            breakpoints: {
+                0: {
+                    slidesPerView: 1,
+                },
+                520: {
+                    slidesPerView: 2,
+                },
+                950: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+        
+        var swiper1 = new Swiper(".slide-content2", {
+            slidesPerView: 3,
+            spaceBetween: 25,
+            loop: false,
+            centerSlide: 'true',
+            centerInsufficientSlides: 'true',
+            fade: 'true',
+            grabCursor: 'true',
+            autoHeight: 'true',
+            pagination: {
+                el: ".swiper-pagination2",
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next2",
+                prevEl: ".swiper-button-prev2",
+            },
+
+            breakpoints: {
+                0: {
+                    slidesPerView: 1,
+                },
+                520: {
+                    slidesPerView: 2,
+                },
+                950: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+        
+        var swiper1 = new Swiper(".slide-content3", {
+            slidesPerView: 3,
+            spaceBetween: 25,
+            loop: false,
+            centerSlide: 'true',
+            centerInsufficientSlides: 'true',
+            fade: 'true',
+            grabCursor: 'true',
+            autoHeight: 'true',
+            pagination: {
+                el: ".swiper-pagination3",
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next3",
+                prevEl: ".swiper-button-prev3",
+            },
+
+            breakpoints: {
+                0: {
+                    slidesPerView: 1,
+                },
+                520: {
+                    slidesPerView: 2,
+                },
+                950: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+        
+        var swiper1 = new Swiper(".slide-content4", {
+            slidesPerView: 3,
+            spaceBetween: 25,
+            loop: false,
+            centerSlide: 'true',
+            centerInsufficientSlides: 'true',
+            fade: 'true',
+            grabCursor: 'true',
+            autoHeight: 'true',
+            pagination: {
+                el: ".swiper-pagination4",
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next4",
+                prevEl: ".swiper-button-prev4",
+            },
+
+            breakpoints: {
+                0: {
+                    slidesPerView: 1,
+                },
+                520: {
+                    slidesPerView: 2,
+                },
+                950: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+        
+        var swiper1 = new Swiper(".slide-content5", {
+            slidesPerView: 3,
+            spaceBetween: 25,
+            loop: false,
+            centerSlide: 'true',
+            centerInsufficientSlides: 'true',
+            fade: 'true',
+            grabCursor: 'true',
+            autoHeight: 'true',
+            pagination: {
+                el: ".swiper-pagination5",
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next5",
+                prevEl: ".swiper-button-prev5",
+            },
+
+            breakpoints: {
+                0: {
+                    slidesPerView: 1,
+                },
+                520: {
+                    slidesPerView: 2,
+                },
+                950: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+        
+        var swiper1 = new Swiper(".slide-content6", {
+            slidesPerView: 3,
+            spaceBetween: 25,
+            loop: false,
+            centerSlide: 'true',
+            centerInsufficientSlides: 'true',
+            fade: 'true',
+            grabCursor: 'true',
+            autoHeight: 'true',
+            pagination: {
+                el: ".swiper-pagination6",
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next6",
+                prevEl: ".swiper-button-prev6",
+            },
+
+            breakpoints: {
+                0: {
+                    slidesPerView: 1,
+                },
+                520: {
+                    slidesPerView: 2,
+                },
+                950: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+        
+        var swiper1 = new Swiper(".slide-content7", {
+            slidesPerView: 3,
+            spaceBetween: 25,
+            loop: false,
+            centerSlide: 'true',
+            centerInsufficientSlides: 'true',
+            fade: 'true',
+            grabCursor: 'true',
+            autoHeight: 'true',
+            pagination: {
+                el: ".swiper-pagination7",
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next7",
+                prevEl: ".swiper-button-prev7",
+            },
+
+            breakpoints: {
+                0: {
+                    slidesPerView: 1,
+                },
+                520: {
+                    slidesPerView: 2,
+                },
+                950: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+        
+        var swiper1 = new Swiper(".slide-content8", {
+            slidesPerView: 3,
+            spaceBetween: 25,
+            loop: false,
+            centerSlide: 'true',
+            centerInsufficientSlides: 'true',
+            fade: 'true',
+            grabCursor: 'true',
+            autoHeight: 'true',
+            pagination: {
+                el: ".swiper-pagination8",
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next8",
+                prevEl: ".swiper-button-prev8",
+            },
+
+            breakpoints: {
+                0: {
+                    slidesPerView: 1,
+                },
+                520: {
+                    slidesPerView: 2,
+                },
+                950: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+        
+        var swiper1 = new Swiper(".slide-content9", {
+            slidesPerView: 3,
+            spaceBetween: 25,
+            loop: false,
+            centerSlide: 'true',
+            centerInsufficientSlides: 'true',
+            fade: 'true',
+            grabCursor: 'true',
+            autoHeight: 'true',
+            pagination: {
+                el: ".swiper-pagination9",
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next9",
+                prevEl: ".swiper-button-prev9",
+            },
+
+            breakpoints: {
+                0: {
+                    slidesPerView: 1,
+                },
+                520: {
+                    slidesPerView: 2,
+                },
+                950: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+        
+        var swiper1 = new Swiper(".slide-content10", {
+            slidesPerView: 3,
+            spaceBetween: 25,
+            loop: false,
+            centerSlide: 'true',
+            centerInsufficientSlides: 'true',
+            fade: 'true',
+            grabCursor: 'true',
+            autoHeight: 'true',
+            pagination: {
+                el: ".swiper-pagination10",
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next10",
+                prevEl: ".swiper-button-prev10",
+            },
+
+            breakpoints: {
+                0: {
+                    slidesPerView: 1,
+                },
+                520: {
+                    slidesPerView: 2,
+                },
+                950: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+        
+        var swiper1 = new Swiper(".slide-content11", {
+            slidesPerView: 3,
+            spaceBetween: 25,
+            loop: false,
+            centerSlide: 'true',
+            centerInsufficientSlides: 'true',
+            fade: 'true',
+            grabCursor: 'true',
+            autoHeight: 'true',
+            pagination: {
+                el: ".swiper-pagination11",
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next11",
+                prevEl: ".swiper-button-prev11",
             },
 
             breakpoints: {
